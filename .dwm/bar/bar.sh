86dde2fa81358ba70cd69d22c2031396219970c5
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2021-12-14 08:52:04 +0530

    change checkupdates timer

--- .dwm/bar/bar.sh
+++ .dwm/bar/bar.sh
@@ -67,7 +67,7 @@ clock() {
 
 while true; do
 
-	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+	[ $interval = 0 ] || [ $(($interval % 1800)) = 0 ] && updates=$(pkg_updates)
 	interval=$((interval + 1))
 
 	sleep 1 && xsetroot -name "$updates      $(battery)      $(brightness)      $(cpu)      $(mem)      $(clock)      "
