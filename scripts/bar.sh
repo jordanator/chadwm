a787409a7323966c6f5481db90971769b38bb975
Lalith Tadigadapa <jordanator@users.noreply.github.com>
2022-04-29 09:42:47 +0530

    checkupdate timer update

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -100,7 +100,7 @@ vol() {
 
 while true; do
 
-	[ $interval = 0 ] || [ $(($interval % 600)) = 0 ] && updates=$(pkg_updates)
+	[ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
 	interval=$((interval + 1))
 
   sleep 1 && xsetroot -name "$updates  $(battery)  $(brightness) $(cpu)  $(mem)  $(clock)  "
